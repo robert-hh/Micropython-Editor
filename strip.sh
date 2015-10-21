aab9d354a714870537b37e9852230f8966139cd1
Robert HH <robert@hammelrath.com>
2015-10-21 18:03:40 +0200

    added line for Wipy

--- strip.sh
+++ strip.sh
@@ -1,6 +1,9 @@
 # !sh
+cpp -D BASIC -D WIPY -D DEFINES pye.py | sed "s/#.*$//" | sed "/^$/d" >wipye.py
 cpp -D BASIC -D PYBOARD -D DEFINES pye.py | sed "s/#.*$//" | sed "/^$/d" >pemin.py
 cpp -D PYBOARD -D DEFINES pye.py | sed "s/#.*$//" | sed "/^$/d" >pe.py
 cpp -D LINUX pye.py | grep -v "^# .*$" >pex.py
 cpp -D PYBOARD pye.py | grep -v "^# .*$" >peb.py
+cat shebang pex.py >pye
+chmod +x pye
 
