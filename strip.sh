5f70fae2ad9c58159db304bdabab8bc721210932
Robert HH <robert@hammelrath.com>
2017-12-23 17:58:59 +0100

    strip.sh: Simplify the script

--- strip.sh
+++ strip.sh
@@ -1,17 +1,14 @@
 # !sh
 #
 cpp -D MICROPYTHON pye2.py | sed "s/#.*$//" | sed "/^$/d" >pye2_mp.py
-cpp -D LINUX       pye2.py | sed "s/#.*$//" | sed "/^$/d" >pex2.py
-cat shebang pex2.py >pye2
+cat shebang <(cpp -D LINUX       pye2.py | sed "s/#.*$//" | sed "/^$/d") >pye2
 chmod +x pye2
-rm pex2.py
 #
 cpp -D MICROPYTHON pye.py | sed "s/#.*$//" | sed "/^$/d" >pye_mp.py
-cpp -D LINUX       pye.py | sed "s/#.*$//" | sed "/^$/d" >pex.py
-cat shebang pex.py >pye
+cat shebang <(cpp -D LINUX       pye.py | sed "s/#.*$//" | sed "/^$/d") >pye
 chmod +x pye
-rm pex.py
 mpy-cross -o wipye.mpy pye_mp.py
 #
+cpp -D MICROPYTHON pye_lazy.py | sed "s/#.*$//" | sed "/^$/d" >pye_mpl.py
 
 
