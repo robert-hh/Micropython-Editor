2be51d8533c1994ef00dd307e7892c3122224612
robert <robert@hammelrath.com>
2019-10-15 17:13:48 +0200

    pye: Remove pye2 files form the master Branch
    pye2 is no a separate branch.

--- strip.sh
+++ strip.sh
@@ -1,9 +1,5 @@
 # !sh
 #
-cpp -D MICROPYTHON pye2.py | sed "s/#.*$//" | sed "/^$/d" >pye2_mp.py
-cat shebang <(cpp -D LINUX       pye2.py | sed "s/#.*$//" | sed "/^$/d") >pye2
-chmod +x pye2
-#
 cpp -D MICROPYTHON pye.py | sed "s/#.*$//" | sed "/^$/d" >pye_mp.py
 cat shebang <(cpp -D LINUX       pye.py | sed "s/#.*$//" | sed "/^$/d") >pye
 chmod +x pye
