b7a5629c402f913860941f1c2c68e607c754ab25
robert <robert@hammelrath.com>
2019-10-15 17:09:56 +0200

    pye2.py: create branch for pye2 and rm pye files

--- strip.sh
+++ strip.sh
@@ -4,8 +4,3 @@ cpp -D MICROPYTHON pye2.py | sed "s/#.*$//" | sed "/^$/d" >pye2_mp.py
 cat shebang <(cpp -D LINUX       pye2.py | sed "s/#.*$//" | sed "/^$/d") >pye2
 chmod +x pye2
 #
-cpp -D MICROPYTHON pye.py | sed "s/#.*$//" | sed "/^$/d" >pye_mp.py
-cat shebang <(cpp -D LINUX       pye.py | sed "s/#.*$//" | sed "/^$/d") >pye
-chmod +x pye
-mpy-cross -o pye_mp.mpy pye_mp.py
-#
